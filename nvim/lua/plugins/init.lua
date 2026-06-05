3c721aa45fbfab9fa39bf37dc9905a337471b4a4
Amin Gholizad <amin.gholizad@gmail.com>
2026-06-05 10:17:38 +0200

    fixed cmake mappings and added telescope integrations

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -16,12 +16,32 @@ return {
     },
     {
       "Civitasv/cmake-tools.nvim",
-      -- lazy = false,
+      ft = { "c", "cpp", "objc", "objcpp", "cuda" },
       dependencies = { "nvim-lua/plenary.nvim" },
       opts = {
         cmake_build_directory = "build", 
         cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
       },
+      config = function(_, opts)
+        require("cmake-tools").setup(opts)
+
+        local map = vim.keymap.set
+
+        map("n", "<leader>cb", "<cmd>CMakeBuild<cr>", { desc = "CMake Build current target" })
+        map("n", "<leader>cr", "<cmd>CMakeRun<cr>", { desc = "CMake Run current target" })
+        map("n", "<leader>cg", "<cmd>CMakeGenerate<cr>", { desc = "CMake Generate build system" })
+        map("n", "<leader>cc", "<cmd>CMakeClean<cr>", { desc = "CMake Clean build directory" })
+        map("n", "<leader>ct", "<cmd>CMakeSelectBuildTarget<cr>", { desc = "CMake Select Build Target" })
+        map("n", "<leader>cx", "<cmd>CMakeSelectLaunchTarget<cr>", { desc = "CMake Select Launch/Run Target" })
+        map("n", "<leader>cm", "<cmd>CMakeSelectBuildType<cr>", { desc = "CMake Select Build Type (Debug/Release)" })
+        map("n", "<leader>co", "<cmd>CMakeOpenExecuter<cr>", { desc = "CMake Open console/output panel" })
+        map("n", "<leader>cq", "<cmd>CMakeCloseExecuter<cr>", { desc = "CMake Close console/output panel" })
+        
+        map("n", "<leader>fca", "<cmd>Telescope cmake_tools<cr>", { desc = "Telescope cmake project files" })
+        map("n", "<leader>fcs", "<cmd>Telescope cmake_tools sources<cr>", { desc = "Telescope cmake project source files" })
+        map("n", "<leader>fcs", "<cmd>Telescope cmake_tools cmake_files<cr>", { desc = "Telescope cmake project model files" })
+        map("n", "<leader>fct", "<cmd>CMakeShowTargetFiles<cr>", { desc = "Telescope cmake target files" })
+      end,
     },
     {
       "mfussenegger/nvim-dap",
@@ -52,7 +72,6 @@ return {
     },
     {
       'MagicDuck/grug-far.nvim',
-      lazy = true,
       cmd = "GrugFar",
       config = function()
         require('grug-far').setup({})

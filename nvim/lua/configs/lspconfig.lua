49d37061864a6c5944898245012f8f3c3599cc3c
Amin Gholizad <amin.gholizad@gmail.com>
2026-08-06 13:18:20 +0200

    add pyright and clang to the lsp server list

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "pyright", "clangd"}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
@@ -16,6 +16,3 @@ vim.lsp.config("clangd", {
     "--fallback-style=llvm",
   },
 })
-
-vim.lsp.enable("clangd")
-
